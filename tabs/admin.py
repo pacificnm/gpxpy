9a846d86107dcf6b46cbaf10b398861735645786
Jaimie Garner <pacificnm@gmail.com>
2025-06-08 20:10:54 +0000

    Resolves Create Album model with slug (FK to Artist) #9

--- tabs/admin.py
+++ tabs/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Artist
+from .models import Album
 
 # Artist
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ("name", "slug")
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("artist", "title", "slug")
