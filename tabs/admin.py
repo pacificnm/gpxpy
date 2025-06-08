72c8137108bc91daeeec8233f8e63f723460e01c
Jaimie Garner <pacificnm@gmail.com>
2025-06-08 20:17:31 +0000

    Resolves Create Song model with slug (FK to Album, file paths, uploader) #10

--- tabs/admin.py
+++ tabs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Artist
 from .models import Album
+from .models import Song
 
 # Artist
 @admin.register(Artist)
@@ -8,7 +9,14 @@ class ArtistAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ("name", "slug")
 
+# Album
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ("artist", "title", "slug")
+
+# Song
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "album", "uploaded_by", "uploaded_at")
